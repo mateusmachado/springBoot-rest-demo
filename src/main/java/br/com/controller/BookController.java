4ab3067e642e367bc60429dc81c1e6ce600a1209
Mateus <mmachado.csp@gmail.com>
2015-07-23 17:34:59 -0300

    Controller Main

---
+++ src/main/java/br/com/controller/BookController.java
@@ -0,0 +1,39 @@
+package br.com.controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.model.Book;
+import br.com.repository.BookRepository;
+
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Map<String, Object> createBook(@RequestBody Map<String, Object> bookMap) {
+		Book book = new Book(bookMap.get("name").toString(), bookMap.get("isbn").toString(),
+				bookMap.get("author").toString(), Integer.parseInt(bookMap.get("pages").toString()));
+
+		bookRepository.save(book);
+		Map<String, Object> response = new LinkedHashMap<String, Object>();
+		response.put("message", "Book created successfully");
+		response.put("book", book);
+		return response;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{bookId}")
+	public Book getBookDetails(@PathVariable("bookId") String bookId) {
+		return bookRepository.findOne(bookId);
+	}
+}
